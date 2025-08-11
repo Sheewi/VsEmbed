c386441473712c643bfdb105da3610d869c940a8
Sheewi <shelbyewing53@gmail.com>
2025-08-11 16:54:06 -0500

    .

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -143,5 +143,12 @@ module.exports = {
 	devServer: {
 		port: 3000,
 		hot: true,
+		static: {
+			directory: path.join(__dirname, '.webpack/renderer'),
+		},
+		compress: true,
+		open: false,
+		historyApiFallback: true,
+		allowedHosts: 'all',
 	}
 };
